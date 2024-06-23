d4555928c1872436a46d3a6c9a85f68e92c02422
zenonet <78077158+zenonet@users.noreply.github.com>
2024-06-23 19:41:28 +0200

    Hopefully fixed build tool

--- BuildTool/Program.cs
+++ BuildTool/Program.cs
@@ -5,6 +5,6 @@ Directory.CreateDirectory(path);
 File.Copy("../../../../Generator/base.html", Path.Join(path, "index.html"), true);
 string src = File.ReadAllText("../../../../Generator/src.wcp");
 string b = File.ReadAllText("../../../../Generator/base.html");
-b = b.Replace("{{srcGoesHere}}", WebUtility.HtmlEncode(src));
+b = b.Replace("{{srcGoesHere}}", src);
 File.WriteAllText(Path.Join(path, "index.html"), b);
-File.Copy("../../../../Runtime/bin/bootsharp/", Path.Join(path, "bootsharp"), true);
+//File.Copy("../../../../Runtime/bin/bootsharp/", Path.Join(path, "bootsharp"), true);
