89d9e4d62b48fe91d1e1a411d7af2a4ce917dd7d
zenonet <78077158+zenonet@users.noreply.github.com>
2024-06-23 16:15:57 +0200

    Added a wasm project for the runtime

---
+++ Runtime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Bootsharp;
+
+public static partial class Program
+{
+    public static void Main ()
+    {
+        OnMainInvoked($"Hello {GetFrontendName()}, .NET here!");
+    }
+
+    [JSEvent] // Used in JS as Program.onMainInvoked.subscribe(..)
+    public static partial void OnMainInvoked (string message);
+
+    [JSFunction] // Set in JS as Program.getFrontendName = () => ..
+    public static partial string GetFrontendName ();
+
+    [JSInvokable] // Invoked from JS as Program.GetBackendName()
+    public static string GetBackendName () => Environment.Version.ToString();
+    
+    [JSFunction] // Set in JS as Program.applyRecomposition = () => ..
+    public static partial string ApplyRecomposition (string newBody);
+}
