c139ccddc295b88761aba564ce923bebde0e6c87
zenonet <78077158+zenonet@users.noreply.github.com>
2024-06-23 18:46:10 +0200

    Added a build tool project

---
+++ BuildTool/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+string path = "../../../../WebCompositionApp";
+Directory.CreateDirectory(path);
+File.Copy("../../../../Generator/base.html", Path.Join(path, "index.html"), true);
+string src = File.ReadAllText("../../../../Generator/src.wcp");
+string b = File.ReadAllText("../../../../Generator/base.html");
+b = b.Replace("{{srcGoesHere}}", WebUtility.HtmlEncode(src));
+File.WriteAllText(Path.Join(path, "index.html"), b);
+File.Copy("../../../../Runtime/bin/bootsharp/", Path.Join(path, "bootsharp"), true);
