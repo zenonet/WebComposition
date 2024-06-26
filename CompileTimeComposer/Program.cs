0f9403678e7097eed031cf31fb040fa46a5b1ac4
zenonet <78077158+zenonet@users.noreply.github.com>
2024-06-26 19:54:16 +0200

    Added CompileTimeComposer.csproj to quickly test the composer

---
+++ CompileTimeComposer/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Diagnostics;
+using Core;
+
+Interpreter i = new();
+string src = File.ReadAllText("../../../../Core/src.wcp");
+
+
+Stopwatch sw = Stopwatch.StartNew();
+var exes = i.ParseExecutables(ref src);
+sw.Stop();
+Console.WriteLine($"Parsing took {sw.Elapsed.TotalMilliseconds}ms");
+
+foreach ((string? key, Variable? value) in VariableSetter.VariableValues)
+{
+    value.VariableChanged += (() =>
+    {
+        Console.WriteLine($"Variable {key} was changed to {value.Value}");
+    });
+}
+sw = Stopwatch.StartNew();
+string html = Composable.ExecuteAndGetHtml(exes);
+sw.Stop();
+Console.WriteLine($"Composition took {sw.Elapsed.TotalMilliseconds}ms");
+File.WriteAllText("../../../../Core/out.html", html);
+Console.WriteLine(html);
