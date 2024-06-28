908c3dc1e30072d72db2c31a98d812c3143d1755
zenonet <78077158+zenonet@users.noreply.github.com>
2024-06-28 21:50:33 +0200

    Added some comments and help to buildtool and completed its functionality for now

--- BuildTool/Program.cs
+++ BuildTool/Program.cs
@@ -1,10 +1,41 @@
-﻿using System.Net;
+﻿
+/*
+Dictionary<string, string?> properties = new();
+for (int i = 0; i < args.Length; i++)
+{
+    if (args[i].StartsWith('-'))
+    {
+        if (args.Length > i+1 && !args[i + 1].StartsWith('-'))
+        {
+            properties[args[i]] = args[++i];
+            continue;
+        }
 
-string path = "../../../../WebCompositionApp";
-Directory.CreateDirectory(path);
-File.Copy("../../../../Core/base.html", Path.Join(path, "index.html"), true);
+        properties[args[i]] = null;
+    }
+}
+*/
+string outputPath = "../../../../WebCompositionApp";
+
+if (args.Length > 0 && args[0] != "editor")
+{
+    Console.WriteLine($"Welcome to the WebCompose build-tool. Modify the code of your app in {Path.GetFullPath("../../../../Core/src.wcp")}.");
+    Console.WriteLine("If you want to build a wwwroot for the web editor, run 'BuildTool.exe editor'");
+    Console.WriteLine($"If you want to build a wwwroot for the app you developed in {Path.GetFullPath("../../../../Core/src.wcp")}, just run the buildtool without any command line arguments");
+    Console.WriteLine($"In both cases the wwwroot will be generated at {Path.GetFullPath(outputPath)}");
+    Console.WriteLine("This is a little messy, I am sorry but I am kind of too lazy to write another command line interface.");
+    Console.WriteLine($"If you need a little more functionality, consider modifying the build tool code at {Path.GetFullPath("../../../Program.cs")}.");
+    return;
+}
+
+Directory.CreateDirectory(outputPath);
+
+// Insert the app source code into the template and copy it to wwwroot
 string src = File.ReadAllText("../../../../Core/src.wcp");
-string b = File.ReadAllText("../../../../Core/base.html");
-b = b.Replace("{{srcGoesHere}}", src);
-File.WriteAllText(Path.Join(path, "index.html"), b);
-//File.Copy("../../../../Runtime/bin/bootsharp/", Path.Join(path, "bootsharp"), true);
+string template = File.ReadAllText(args.Length > 0 && args[0] == "editor" ? "../../../../Core/base.html" : "../../../../Core/standaloneBase.html");
+template = template.Replace("{{srcGoesHere}}", src);
+File.WriteAllText(Path.Join(outputPath, "index.html"), template);
+
+// Copy the runtime wasm lib to wwwroot
+File.Copy("../../../../Runtime/bin/bootsharp/index.mjs", Path.Join(outputPath, "index.js"), true);
+Console.WriteLine($"Successfully created wwwroot at {Path.GetFullPath(outputPath)}");
