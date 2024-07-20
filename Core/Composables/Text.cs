5e2c706bc92469924e10d423790f8042a08f3cec
zenonet <78077158+zenonet@users.noreply.github.com>
2024-07-20 18:48:01 +0200

    Fixed some legacy code in Text.cs and TextField.cs

--- Core/Composables/Text.cs
+++ Core/Composables/Text.cs
@@ -7,9 +7,8 @@ public class Text : Composable
     {
         EnsureParameterCount(1);
         
-        var txt = Parameters[0].Execute();
-        if (txt is IntValue iv) txt = new StringValue{Value = iv.Value.ToString()};
-        if (txt is not StringValue sv) throw new ($"The Text() composable takes a string as the parameter but received {txt.TypeName}");
+        StringValue sv = Parameters[0].Execute().AsString(LineNumber);
+        
         return $"<span class=\"composable text\" {GetStyleStringOrEmpty()}>{sv.Value.Replace("\\n", "<br>")}</span>";
     }
 }
--- Core/Composables/TextField.cs
+++ Core/Composables/TextField.cs
@@ -5,23 +5,15 @@ public class TextField : Composable
     public override string GenerateHtml()
     {
         EnsureParameterCount(2);
-        
-        Value txt = Parameters[0].Execute();
-        if (txt is IntValue iv) txt = new StringValue{Value = iv.Value.ToString()};
-        if (txt is not StringValue sv) throw new ($"The TextField() composable takes a string as it's first parameter but received {txt.TypeName}");
+
+        StringValue sv = Parameters[0].Execute().AsString(LineNumber);
         
         if (Parameters[1].Execute() is not LambdaReferenceValue lambda)
-            throw new ("Text field only accepts strings as their first argument");
+            throw new ("TextField expects a lambda function as it's second parameter.");
 
         string id = GetNewId();
-        string oninput = $"oninput=\"document.callLambdaWithArgument({lambda.FunctionIndex}, document.getElementById('{id}').value)\"";
+        string oninput = $"oninput=\"document.callLambdaWithArgument({lambda.FunctionIndex}, document.getElementById('{id}').value, 'string')\"";
         
-        /*if (Parameters.Count > 1)
-        {
-            // TODO: Add lambda parameters so that this makes sense
-            if (Parameters[1].Execute() is not LambdaReferenceValue lambda) throw new("Unexpected type at 2. parameter of TextField. Should be a lambda function.");
-            onChange = $"onchange=\"document.callLambda({lambda.FunctionIndex})\"";
-        }*/
         return $"<input id=\"{id}\" {GetStyleStringOrEmpty()} class=\"composable textfield\" type=\"text\" {oninput} value=\"{sv.Value}\">";
     }
 }
